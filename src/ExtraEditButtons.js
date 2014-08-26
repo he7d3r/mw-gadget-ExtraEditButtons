5bdd66c0409ec9ca30bf9d73ed977ad4633fab75
Helder <he7d3r@gmail.com>
2014-08-26 17:31:27 -0300

    Remove unnecessary test for preference "showtoolbar"
    The WikiEditor toolbar is also visible when the user disables the preference
    corresponding to the classic edit toolbar, but enables the enhanced one, so
    add the buttons in that case as well.
    
    See:
    https://bugzilla.wikimedia.org/show_bug.cgi?id=30795

--- src/ExtraEditButtons.js
+++ src/ExtraEditButtons.js
@@ -580,7 +580,7 @@ if ( $.inArray( mw.config.get( 'wgAction' ), [ 'edit', 'submit' ] ) !== -1 ) {
 	mw.loader.using( 'user.options', function () {
 		// This can be the string "0" if the user disabled the preference ([[bugzilla:52542#c3]])
 		/*jshint eqeqeq:false*/
-		if ( mw.user.options.get( 'usebetatoolbar' ) == 1 && mw.user.options.get( 'showtoolbar' ) == 1 ) {
+		if ( mw.user.options.get( 'usebetatoolbar' ) == 1 ) {
 			$.when(
 				mw.loader.using( 'ext.wikiEditor.toolbar' ),
 				$.ready
