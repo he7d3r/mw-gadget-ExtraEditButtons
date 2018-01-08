23c66a6688d4390ea31985f0dece1b9c9095f082
Helder <he7d3r@gmail.com>
2018-01-08 14:42:28 -0200

    Update WikiEditor dependency
    See https://phabricator.wikimedia.org/T165112.

--- src/ExtraEditButtons.js
+++ src/ExtraEditButtons.js
@@ -540,7 +540,7 @@
 			/*jshint eqeqeq:false*/
 			if ( mw.user.options.get( 'usebetatoolbar' ) == 1 ) {
 				$.when(
-					mw.loader.using( 'ext.wikiEditor.toolbar' ),
+					mw.loader.using( 'ext.wikiEditor' ),
 					$.ready
 				).then( customizeToolbar );
 			}
